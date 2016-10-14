2091d1c3e55b40e33d4b71ef5e5553c07e19eda6
seanoreilly73 <s.oreilly@linnovations.co.uk>
2016-10-14 20:32:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var toAdd = 23;
 
 function summer (sumVal) {
     return function (toAdd) {
-        if (!toAdd) throw new Error('trying to pull a fast one?');
+        if (!toAdd) throw new Error("trying to pull a fast one?");
         return sumVal + toAdd;
     };
 }
